8390a35b78153ea73321fe393975e2dd7e2c03be
JustinZhang <schnell18@gmail.com>
2019-08-11 20:13:28 +0800

    Add gcd simple project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Introduction
+
+This is a collection mini rust project to practise *RUST* programming.
+
+## Catelog
+
+| sub-directory      | comment                            |
+| ------------------ | ---------------------------------- |
+| gcd                | calculate greatest common divisor  |
---
+++ gcd/src/main.rs
@@ -0,0 +1,28 @@
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m !=0);
+    while m != 0 {
+        if m < n {
+            let t = m; 
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
+}
+
+fn main() {
+    let n = 84;
+    let m = 68;
+    println!("Greatest common diviisor of {} and {} is {}",  n, m, gcd(n, m));
+}
+
+#[test]
+fn test_gcd() {
+    assert_eq!(gcd(14,15), 1);
+
+    assert_eq!(
+        gcd(2 * 3 * 5 * 11 * 17,  3 * 7 * 11 * 13 * 19),
+        3 * 11
+    );
+}
