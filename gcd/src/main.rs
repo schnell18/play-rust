5d88a43ffe3962e0df7548875fadbbdd25bae1b1
JustinZhang <schnell18@gmail.com>
2019-08-11 21:23:06 +0800

    Add CLI arguments parsing

--- gcd/src/main.rs
+++ gcd/src/main.rs
@@ -1,3 +1,24 @@
+use std::io::Write;
+use std::str::FromStr;
+
+fn main() {
+    let mut numbers = Vec::new();
+
+    for arg in std::env::args().skip(1) {
+        numbers.push(u64::from_str(&arg).expect("error parsing argument"));
+    }
+
+    if numbers.len() == 0 {
+        writeln!(std::io::stderr(), "Usage: gcd NUMBER ... ").unwrap();
+        std::process::exit(1);
+    }
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+    println!("Greatest common diviisor of {:?} is {}",  numbers, d);
+}
+
 fn gcd(mut n: u64, mut m: u64) -> u64 {
     assert!(n != 0 && m !=0);
     while m != 0 {
@@ -9,12 +30,6 @@ fn gcd(mut n: u64, mut m: u64) -> u64 {
         m = m % n;
     }
     n
-}
-
-fn main() {
-    let n = 84;
-    let m = 68;
-    println!("Greatest common diviisor of {} and {} is {}",  n, m, gcd(n, m));
 }
 
 #[test]
