c5670931b0737342f441aedbe0efc2bcab0730a5
JustinZhang <schnell18@gmail.com>
2019-08-13 23:43:29 +0800

    Add mandelbrot set functions

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ This is a collection mini rust project to practise *RUST* programming.
 | gcd                | calculate greatest common divisor  |
 | iron-gcd      | gcd w/ web interface |
 | fibonacci    | calculate fibonacci series|
+| mandelbrot    | calculate mandelbrot set|
---
+++ mandelbrot/src/main.rs
@@ -0,0 +1,55 @@
+extern crate num;
+use num::Complex;
+use std::str::FromStr;
+
+fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
+    let mut z = Complex { re: 0.0, im: 0.0};
+    for i in 0..limit {
+        z = z * z + c;
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+    }
+
+    None
+}
+
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
+    match s.find(separator) {
+        None => None,
+        Some(index) => {
+            match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+                (Ok(l), Ok(r)) => Some((l, r)),
+                _ => None
+            }
+        }
+    }
+}
+
+fn parse_complex(s: &str) -> Option<Complex<f64>> {
+    match parse_pair(s, ',') {
+        Some((r, i)) => Some(Complex {re: r, im: i}),
+        None => None
+    }
+}
+
+#[test]
+fn test_parse_pair() {
+    assert_eq!(parse_pair::<u32>("" , ',' ), None);
+    assert_eq!(parse_pair::<u32>("10," , ',' ), None);
+    assert_eq!(parse_pair::<u32>(",10" , ',' ), None);
+    assert_eq!(parse_pair::<u32>("10,20" , ',' ), Some((10, 20)));
+    assert_eq!(parse_pair::<u32>("10,20xy" , ',' ), None);
+    assert_eq!(parse_pair::<f64>("0.5x" , 'x' ), None);
+    assert_eq!(parse_pair::<f64>("0.5x1.5" , 'x' ), Some((0.5, 1.5)));
+}
+
+#[test]
+fn test_parse_complex() {
+    assert_eq!(parse_complex("0.25,0.0625"), Some(Complex {re: 0.25, im: 0.0625}));
+    assert_eq!(parse_complex(",0.0625"), None);
+}
+
+fn main() {
+    println!("Hello, world!");
+} 
