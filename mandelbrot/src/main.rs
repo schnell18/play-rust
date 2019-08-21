3b97e766c5521c6b036a8b9f117445842b2879a5
JustinZhang <schnell18@gmail.com>
2019-08-21 10:05:58 +0800

    Multi-thread version

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,5 +1,7 @@
 extern crate image;
 extern crate num;
+extern crate num_cpus;
+extern crate crossbeam;
 
 use image::ColorType;
 use image::png::PNGEncoder;
@@ -135,7 +137,35 @@ fn main() {
     );
 
     let mut pixels = vec![0; bounds.0 * bounds.1];
+
+    let threads = num_cpus::get();
+    let rows_per_band = bounds.1 / threads + 1;
+
+    writeln!(
+        std::io::stderr(),
+        "Working on {} w/ {} threads", &args[2], threads
+    ).unwrap();
+ 
+    {
+        let bands: Vec<&mut [u8]> = pixels.chunks_mut(rows_per_band * bounds.0).collect();
+        crossbeam::scope(|spawner| {
+            for (i, band) in bands.into_iter().enumerate() {
+                let top = rows_per_band * i;
+                let height = band.len() / bounds.0;
+                let band_bounds = (bounds.0, height);
+                let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
+                let band_lower_right = pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
+
+                spawner.spawn(move || {
+                    render(band, band_bounds, band_upper_left, band_lower_right);
+                });
+            }
+        });
+    }
+
+
     render(&mut pixels, bounds, upper_left, lower_right);
+
     write_image(&args[1], &pixels, bounds).expect(
             "error writing PNG file"
     );
