d7595dc1d57177551a14794b0b39b262148a9974
Justin Zhang <schnell18@gmail.com>
2026-09-21 21:42:51 +1200

    2 threads + main
    Signed-off-by: Justin Zhang <schnell18@gmail.com>

--- tokio/src/main.rs
+++ tokio/src/main.rs
@@ -2,13 +2,14 @@ use tokio::join;
 
 #[tokio::main(worker_threads = 2)]
 async fn main() {
-    let a = get_page("https://news.ycombinator.com");
-    let b = get_page("https://www.lobste.rs");
-    join!(a, b);
+    let a = tokio::spawn(get_page("https://news.ycombinator.com"));
+    let b = tokio::spawn(get_page("https://www.lobste.rs"));
+    let c = get_page("https://www.lobste.rs");
+    let _ = join!(a, b, c);
 }
 
 pub async fn get_page(url: &str) {
     println!("Retrieving {}", url);
-    let _ = ureq::get(url).call().unwrap();
+    let _ = reqwest::get(url).await.unwrap();
     println!("Completed {}", url);
 }
